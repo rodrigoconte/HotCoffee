0324dd71b1e6ed8ce602b85c162e0a6a95e7d647
Rodrigo Conte <rodrigo.conte@ckl.io>
2021-04-25 16:20:21 -0400

    [Feature] Include api integration to read and create new orders

--- HotCoffee/Scenes/AddOrder/AddOrderViewModel.swift
+++ HotCoffee/Scenes/AddOrder/AddOrderViewModel.swift
@@ -11,6 +11,8 @@ struct addOrderViewModel {
     
     var name: String?
     var email: String?
+    var selectedType: String?
+    var selectedSize: String?
     
     var types: [String] {
         return CoffeeType.allCases.map { $0.rawValue.capitalized }
--- HotCoffee/Scenes/Orders/Models/Order.swift
+++ HotCoffee/Scenes/Orders/Models/Order.swift
@@ -28,3 +28,43 @@ struct Order: Codable {
     let size: CoffeeSize
     
 }
+
+extension Order {
+    
+    static var all: Resource<[Order]> = {
+        guard let url = URL(string: "https://guarded-retreat-82533.herokuapp.com/orders") else { fatalError("error while creating URL") }
+        return Resource<[Order]>(url: url)
+    }()
+    
+    init?(_ vm: addOrderViewModel) {
+        guard let name = vm.name,
+              let email = vm.email,
+              let type = CoffeeType(rawValue: vm.selectedType!.lowercased()),
+              let size = CoffeeSize(rawValue: vm.selectedSize!.lowercased()) else { return nil }
+        
+        self.name = name
+        self.email = email
+        self.type = type
+        self.size = size
+    }
+    
+}
+
+extension Order {
+    
+    static func create(vm: addOrderViewModel) -> Resource<Order?> {
+        let order = Order(vm)
+        guard let url = URL(string: "https://guarded-retreat-82533.herokuapp.com/orders") else { fatalError("error while creating URL") }
+        
+        do {
+            let data = try JSONEncoder().encode(order)
+            var resource = Resource<Order?>(url: url)
+            resource.httpMethod = .post
+            resource.body = data
+            return resource
+        } catch {
+            fatalError("Error enconding order")
+        }
+    }
+    
+}
