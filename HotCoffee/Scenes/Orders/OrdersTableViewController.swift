407f05b8015787aeedbd0a8e8a812f37dc97a6d0
Rodrigo Conte <rodrigo.conte@ckl.io>
2021-04-25 14:19:53 -0400

    [Feature] Included view model of orders list
    this commit includes:
    OrderViewModel
    OrderListViewModel
    Display orders on view controller

--- HotCoffee/Scenes/Orders/OrdersTableViewController.swift
+++ HotCoffee/Scenes/Orders/OrdersTableViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class OrdersTableViewController: UITableViewController {
     
+    // MARK: - Attributes
+    
+    var ordersListViewModel = OrderListViewModel()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -21,15 +25,38 @@ class OrdersTableViewController: UITableViewController {
             fatalError("URL was incorrect")
         }
         let resource = Resource<[Order]>(url: coffeeOrdersURL)
-        WebService().load(resource: resource) { result in
+        WebService().load(resource: resource) { [weak self] result in
             switch result {
             case .success(let orders):
-                print(orders)
+                self?.ordersListViewModel.ordersViewModel = orders.map(OrderViewModel.init)
+                self?.tableView.reloadData()
             case .failure(let error):
                 print(error)
             }
         }
+    }
+    
+}
+
+// MARK: - Table view Data source and Delegate
+
+extension OrdersTableViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ordersListViewModel.ordersViewModel.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let vm = self.ordersListViewModel.orderViewModel(at: indexPath.row)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderTableViewCell", for: indexPath)
         
+        cell.detailTextLabel?.text = vm.type
+        cell.textLabel?.text = vm.name
+        return cell
     }
     
 }
